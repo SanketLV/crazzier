9c51d7b70bf684cc9c8221b75d15d4d6c77231d9
SanketLV <sanketlakhani80@gmail.com>
2025-10-16 17:47:40 +0530

    feat: implement TRPC setup with client and server integration

---
+++ app/client.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+
+const Client = () => {
+  const trpc = useTRPC();
+  const { data: users } = useSuspenseQuery(trpc.getUsers.queryOptions());
+
+  return <div>Client Component : {JSON.stringify(users, null, 2)}</div>;
+};
+
+export default Client;
