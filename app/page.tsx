2f164a0caaa3007082eab664e529a9e44b184953
SanketLV <sanketlakhani80@gmail.com>
2025-10-16 22:12:30 +0530

    feat: Refactor authentication and user management
    - Added better-auth package for improved authentication handling.
    - Updated Prisma schema to remove Post and User tables, replacing them with new User, Session, Account, and Verification tables.
    - Implemented new database migration to drop old tables and create new ones.
    - Enhanced TRPC context to include session validation with protected procedures.
    - Updated app router to fetch users based on authenticated session.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Client from "./client";
-import { Suspense } from "react";
+import { requireAuth } from "@/lib/auth-utils";
+import LogoutButton from "./logout";
 
 export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  await requireAuth();
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      Protected Server component
+      <LogoutButton />
     </div>
   );
 }
