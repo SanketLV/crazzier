a6e57b818bff34e20395e4b980fec5f4f92d22bb
SanketLV <sanketlakhani80@gmail.com>
2025-10-18 21:50:13 +0530

    feat: add Workflow model and CRUD operations
    - Created a new Workflow table in the database with id and name fields.
    - Updated the Prisma schema to include the Workflow model.
    - Implemented getWorkflows and createWorkflow procedures in the TRPC router.
    - Integrated Inngest for workflow creation notifications.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import { requireAuth } from "@/lib/auth-utils";
 import LogoutButton from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
 
-export default async function Home() {
-  await requireAuth();
+export default function Home() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       Protected Server component
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
